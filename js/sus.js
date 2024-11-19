2fecf2f1ebd349831c13d10a62d0388cfdcaca5c
tiagolimar <tiagolima.r2@gmail.com>
2024-11-18 21:40:54 -0300

    version with functions

--- js/sus.js
+++ js/sus.js
@@ -1,48 +1,44 @@
-const pacientes = [];
-let continuar = true;
+const pacientes = []
 
-while(continuar){
-    let index;
-    let lista = ''
-    for(let key in pacientes) {lista += `${Number(key)+1}º ${pacientes[key]}\n`}
+function executarMenu(){
+    return prompt(`
+Lista de Pacientes:
 
-    const op = prompt(`
-        Lista de Pacientes:
-\n${lista}
-        1 - Cadastrar usuário
-        2 - Editar usuário
-        3 - Excluir usuário
-        4 - Sair
+${pacientes.map((paciente,key)=> `${Number(key)+1}º ${paciente}\n`).join('\n')}
+
+1 - Cadastrar usuário
+2 - Editar usuário
+3 - Excluir usuário
+4 - Sair
     `)
-
-    console.log(op);
-
-    switch(op){
-        case '1': 
-            pacientes.push(prompt('Digite o nome do paciente:'));
-            break;
-        case '2': 
-            index = Number(prompt('Digite o número do paciente que deseja editar:'))-1;
-            if (index < 0 || index >= pacientes.length) {
-                alert('Paciente não encontrado!');
-                break;
-            }
-            pacientes[index] = prompt('Digite o novo nome do paciente:');
-            break;
-        case '3': 
-            index = Number(prompt('Digite o número do paciente que deseja excluir:'))-1;
-            if (index < 0 || index >= pacientes.length) {
-                alert('Paciente não encontrado!');
-                break;
-            }
-            pacientes.splice(index, 1);
-            break;
-        case false:
-        case '4': 
-            alert('Até logo!');
-            continuar = false;
-            break;
-        default:
-            undefined;
-    }
 }
+
+function cadastrar(){
+    pacientes.push(prompt('Digite o nome do paciente:'));
+    main();
+}
+
+function editar(){
+    const index = Number(prompt('Digite o número do paciente que deseja editar:'))-1
+    pacientes[index] = prompt('Digite o novo nome do paciente:')
+    main();
+}
+
+function excluir(){
+    const index = Number(prompt('Digite o número do paciente que deseja excluir:'))-1
+    pacientes.splice(index, 1)
+    main();
+}
+
+function main(){
+    const controller = {
+        1: cadastrar,
+        2: editar,
+        3: excluir,
+        4: alert('Até logo!')
+    }
+
+    controller[executarMenu()]();
+}
+
+main();
